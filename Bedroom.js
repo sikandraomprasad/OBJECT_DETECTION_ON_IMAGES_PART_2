45538bfacb346da82c5ebfe41859d511ff98d9f0
sikandraomprasad <89696118+sikandraomprasad@users.noreply.github.com>
2023-05-16 14:40:17 +0530

    Add files via upload

---
+++ Bedroom.js
@@ -0,0 +1,30 @@
+Status = "";
+bedroom_image = "";
+
+function preload(){
+    bedroom_image = loadImage("Bedroom.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(bedroom_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(bedroom_image,0,0,640,350);
+}
---
+++ TVandAC.js
@@ -0,0 +1,31 @@
+Status = "";
+TVandAc_image = "";
+
+function preload(){
+    TVandAc_image = loadImage("TV and Ac.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(TVandAc_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(TVandAc_image,0,0,640,350);
+}
